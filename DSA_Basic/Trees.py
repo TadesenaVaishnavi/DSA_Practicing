45b9ba8064de3c45cf4daf84f4ddae92191fb667
TadesenaVaishnavi <160591205+TadesenaVaishnavi@users.noreply.github.com>
2025-09-02 23:44:01 +0530

    Update Trees.py

--- DSA_Basic/Trees.py
+++ DSA_Basic/Trees.py
@@ -96,3 +96,80 @@ def invertTreeIterative(root):
 
 # ⏱️ Time Complexity: O(N)
 # 📦 Space Complexity: O(N) (queue can hold all nodes at worst level).
+
+
+# New topic
+# Binary Search Trees (BSTs)
+         
+# Create & Insert into BST
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+# 1) Insert into BST
+def insert(root, val):
+    if not root:
+        return Node(val)
+    if val < root.val:
+        root.left = insert(root.left, val)
+    else:
+        root.right = insert(root.right, val)
+    return root
+# 2) Height of a Binary Tree
+# in tree they is answer
+         
+# 3) Diameter of a Binary Tree
+def diameter(root):
+    def dfs(node):
+        nonlocal max_diameter
+        if not node:
+            return 0
+        
+        # heights of left & right subtrees
+        left_height = dfs(node.left)
+        right_height = dfs(node.right)
+        
+        # diameter passing through this node
+        max_diameter = max(max_diameter, left_height + right_height)
+        
+        return 1 + max(left_height, right_height)
+    
+    max_diameter = 0
+    dfs(root)
+    return max_diameter
+
+                
+       1
+      / \
+     2   3
+    / \
+   4   5
+# Longest path = 4 → 2 → 1 → 3 (or 5 → 2 → 1 → 3)
+# Diameter = 3 edges (or 4 nodes)
+
+# 4) Lowest Common Ancestor (LCA)
+def lca_bst(root, p, q):
+    if not root:
+        return None
+    if p < root.val and q < root.val:
+        return lca_bst(root.left, p, q)
+    elif p > root.val and q > root.val:
+        return lca_bst(root.right, p, q)
+    else:
+        return root
+                    
+# Case 2: In a General Binary Tree
+# Here, we can’t use BST property.
+def lca_binary_tree(root, p, q):
+    if not root or root.val == p or root.val == q:
+        return root
+    
+    left = lca_binary_tree(root.left, p, q)
+    right = lca_binary_tree(root.right, p, q)
+    
+    if left and right:
+        return root
+    return left if left else right
+
