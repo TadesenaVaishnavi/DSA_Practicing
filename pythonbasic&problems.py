5255ca727dda7a0b85af224cd96d1ec018dd809f
TadesenaVaishnavi <160591205+TadesenaVaishnavi@users.noreply.github.com>
2025-11-25 17:39:56 +0530

    Create pythonbasic&problems.py
    new file added

---
+++ pythonbasic&problems.py
@@ -0,0 +1,9 @@
+WHy f"{num}-{ch}"?
+f"" -- f-string (formatting enabled)
+The f before the string allows you to write variables inside {} directly.
+
+other way
+1)str(num) + "-" + ch --- "" or '' okay
+2) "{}-{}".format(num, ch)
+
+for space ( - ) give direct space no need to add anything
