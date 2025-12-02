8cd7bc2b0594aae5f8de781b64b8741a21b76d63
TadesenaVaishnavi <160591205+TadesenaVaishnavi@users.noreply.github.com>
2025-12-02 15:11:42 +0530

    Update Arrays.py

--- DSA_Basic/Arrays1.py
+++ DSA_Basic/Arrays1.py
@@ -75,9 +75,53 @@ arr.pop(k)
 print(arr)
 
 # Reverse the Array
+# Reverse an Array (Without using built-in reverse)
+arr = [1,4,7,3,6,2]
+print(arr)
 
+i = 0
+j = len(arr) -1
+while i < j:
+    arr[i], arr[j] = arr[j], arr[i]
+    i += 1
+    j -= 1
+print(arr)
+
+
+# with built-in
 n = int(input())
 arr = list(map(int, input().split()))
 arr.reverse()
 print(*arr)
 
+
+# Find Max & Min in an Array
+
+arr = [1,3,6,-1,6,33]
+maxi = mini = arr[0]
+for num in arr:
+    if num > maxi:
+        maxi = num
+    if num < maxi:
+        mini = num
+print("max:", maxi)
+print("min:", mini)
+
+# Two-Sum (Easy Version) 
+# Given array + target → return indexes of two numbers that add up to target.
+# Example:
+# arr = [2, 7, 11, 15], target = 9 → output = [0, 1] -- 2+7 = 9
+
+# Approach 1 — Brute Force (Beginner Friendly)
+# Check all pairs → O(n²)
+arr = [2, 7, 11, 15]
+target = 9
+
+for i in range(len(arr)):
+    for j in range(i+1, len(arr)):
+        if arr[i] + arr[j] == target:
+            print([i, j])
+            break
+# Approach 2 — Hash Map (Best Logic for Interviews)
+# Use dictionary to store {value: index}
+# For each element check: target - arr[i] already seen?
