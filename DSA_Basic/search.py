fa89b3a19d28f182eb6b542fc6714125ddbf7006
TadesenaVaishnavi <160591205+TadesenaVaishnavi@users.noreply.github.com>
2026-01-02 11:39:32 +0530

    Create search.py
    new file add in DSA_practicing

---
+++ DSA_Basic/search.py
@@ -0,0 +1,33 @@
+# binarysearch
+
+def binarysearch(list1, key):
+    low = 0
+    high = len(list1) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if key == list1[mid]:
+            return True
+        elif key > list1[mid]:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return False
+list1 = [2,5,3,4,1,3]
+list1.sort()
+key = 1
+binarysearch(list1, key)
+
+
+ # linearsearch
+
+def linearsearch(list1, key):
+    for i in range(len(list1)):
+        if list1[i] == key:
+            return True
+    return False
+list1 = [2,5,3,4,1,3]
+key = 1
+linearsearch(list1, key)
